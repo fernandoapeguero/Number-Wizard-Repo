cbc1155ea638fd1b005815cfefa73736ef8c8c1c
Fernando peguero <fernandoapeguero@gmail.com>
2018-11-09 13:51:09 -0500

    Initial commit

---
+++ Number Wizard UI/Assets/Script/NumberWizard.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+    [SerializeField] TextMeshProUGUI textContainerGuess;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        StarGame();
+	}
+
+    void StarGame() {
+       
+        Guessed();
+        
+    }
+	
+	// Update is called once per frame
+
+    public void OnPressHigher() {
+        if (guess + 1 <= max) {
+            min = guess + 1;
+            Guessed();
+        }
+
+    }
+
+    public void OnPressLower() {
+        if (guess - 1 >= min) {
+
+            max = guess - 1;
+            Guessed();
+        }
+        
+    }
+
+    void Guessed() {
+            guess = Random.Range(min , max + 1) ;
+            textContainerGuess.text = guess.ToString();
+
+    }
+}
